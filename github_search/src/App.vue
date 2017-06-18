c75a812e0d91605fbde0b734ad36183732bde619
Xingyu Gu <xingyuguseu@gmail.com>
2017-06-18 16:58:39 -0700

    add github_search

---
+++ github_search/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import {router} from './bootstrap';
+export default {
+  name: 'github_search',
+  router,
+
+
+}
+</script>
+
